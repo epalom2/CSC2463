878e490dc661d6949f202c1d6d059e319b51c9c2
epalom2 <epalom2@lsu.edu>
2024-01-23 16:28:52 -0600

    assignment 1
    first p5.js assignment

--- sketch.js
+++ sketch.js
@@ -1,13 +1,38 @@
 function setup() {
-  createCanvas(500, 250);
-  background(0 , 255, 0);
+  
+  createCanvas(1000, 1000);
+  background(225);
+  
+  
+  
 }
 
 function draw() {
-  
-  circle(125, 120, 210);
-  
-  square(275, 25, 200);
+  //example 1
+  fill(0, 225, 0)
+  rect(10, 10, 290, 150);
+ 
+  fill(255);
+  circle(85, 85, 130);
+
+  fill(255);
+  square(165, 25, 120);
+
+  //example 2
+  noStroke();
+  fill(255);
+  square(10, 200, 300);
+
+  fill(255, 0, 0, 100);
+  circle(160, 305, 150);
+
+  fill(0, 0, 255, 100);
+  circle(110, 400, 150);
+
+  fill(0, 255, 0, 100);
+  circle(210, 400, 150);
 }
+
+
 
 
