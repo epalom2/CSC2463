2e26ff0e705299a329625bfe7aedb63d80fa7b1f
epalom2 <epalom2@lsu.edu>
2024-01-23 14:27:07 -0600

    first sketch example

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(222);
+  circle(250, 250, 500);
+  circle(150, 150, 50);
+  circle(350, 150, 50);
+  line( 75, 300, 350, 250 )
+}
